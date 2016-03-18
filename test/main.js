641d7c8268dd4c2dddae833fa577479c05676bed
Kirill Lassounski <kirillsk8@gmail.com>
2016-03-18 09:47:39 -0300

    adding more main test programs

--- test/main.js
+++ test/main.js
@@ -5,10 +5,10 @@ var env = require('../app/environment');
 describe('Main runner test', function () {
     it('should declare two variables and sum them', function () {
         var program = 'do(define(x,1),define(y,6),+(x,y))';
-        expect(main.run([program],env)).to.equal(7);
+        expect(main.run([program], env)).to.equal(7);
     });
-    
-    it('should declare a function and call it', function() {
+
+    it('should declare a sum function and call it', function () {
         var program = [
             'do(',
             '   define(sum,',
@@ -17,7 +17,18 @@ describe('Main runner test', function () {
             '   print(sum(1,6))',
             ')'
         ];
-        expect(main.run(program,env)).to.equal(7);
+        expect(main.run(program, env)).to.equal(7);
+    });
+
+    it('should declare a power function and call it', function () {
+        var program = [
+            "do(define(pow, fun(base, exp,",
+            "     if(==(exp, 0),",
+            "        1,",
+            "        *(base, pow(base, -(exp, 1)))))),",
+            "   print(pow(2, 10)))"
+        ];
+        expect(main.run(program, env)).to.equal(1024);
     });
 
 });
